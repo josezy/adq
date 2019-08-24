18df5385f4cb683bc4594d2d07050500645524ff
JoseB <jose.zdy@gmail.com>
2019-08-24 12:56:43 -0500

    refactor code layout

--- prueba_sensor/prueba_sensor.ino
+++ prueba_sensor/prueba_sensor.ino
@@ -1,47 +1,49 @@
-
-/*
-  This example uses only 4 wires to test the BH1750 Light sensor
- 
-  Connecting the sensor to a Arduino UNO:
-  VCC  <-> 3V3
-  GND  <-> GND
-  SDA  <-> A4/SDA 
-  SCL  <-> A5/SCL
-*/
-
-#include <BH1750FVI.h>
 #include <Arduino_JSON.h>
-#define INPUT_SIZE 30
 
+#define s_BH1750FVI true    // used pines: A4, A5
+#define s_MQ135     true    // used pines: A0
 
-// Create the Lightsensor instance
-BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+#if s_BH1750FVI
+  #include <BH1750FVI.h>
+  BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+#endif
 
-long dt=60; //MilliSecons
-bool SendJson=false;
-long time_to_send=millis();
-JSONVar Sensor,Data,Units;
-void setup() 
-{
+long dt = 1000; // milliseconds
+long last_now = millis();
+JSONVar Data;
+
+void setup(){
   Serial.begin(115200);
-  LightSensor.begin();  
-  Units["luz"]="LUMENS";
-  Sensor["ts"]=1; 
-  Sensor["luz"]=1;
-  Sensor["units"]=Units;
-  Data["BH1750FVI"]=Sensor;
+
+  #if s_BH1750FVI
+    LightSensor.begin();
+    Data["BH1750FVI"]["units"]["light"] = "lumen";
+  #endif
+  #if s_MQ135
+    Data["MQ135"]["units"]["air_ppm"] = "ppm";
+  #endif
 }
 
-void loop()
-{
-  uint16_t lux = LightSensor.GetLightIntensity();
-  if(millis()-time_to_send>dt){  
-    Serial.flush();  
-    time_to_send=millis();
-    Sensor["luz"]=(String) lux;
-    Data["BH1750FVI"]=Sensor;
+void loop(){
+  #if s_BH1750FVI
+    uint16_t lux = LightSensor.GetLightIntensity();
+  #endif
+
+  #if s_MQ135
+    int air_ppm = analogRead(A0);
+  #endif
+
+  long now = millis();
+  if(now - last_now > dt){
+    last_now = now;
+
+    #if s_BH1750FVI
+      Data["BH1750FVI"]["light"] = (long unsigned int)lux;
+    #endif
+    #if s_MQ135
+      Data["MQ135"]["air_ppm"] = air_ppm;
+    #endif
+
     Serial.println(JSON.stringify(Data));
-    
   }
-  
 }
