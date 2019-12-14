e60e5a18da4f45b2df00305eb918666cd079c3ae
Jose B <jose.zdy@gmail.com>
2019-12-14 16:50:40 -0500

    add dust sensor

--- sensor_serial/sensor_serial.ino
+++ sensor_serial/sensor_serial.ino
@@ -2,10 +2,20 @@
 
 #define s_BH1750FVI true    // used pines: A4, A5
 #define s_MQ135     true    // used pines: A0
+#define s_KS0196    true    // used pines: A1, D0
 
 #if s_BH1750FVI
   #include <BH1750FVI.h>
   BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
+#endif
+
+#if s_KS0196
+  int ks0196_analog_pin = A1;
+  int ks0196_digital_pin = D0;
+  int ks0196_t1 = 280;
+  int ks0196_t2 = 9620;
+  int N = 1;
+  float mean = 0; 
 #endif
 
 long dt = 1000; // milliseconds
@@ -22,6 +32,11 @@ void setup(){
   #if s_MQ135
     Data["MQ135"]["air_ppm"]["units"] = "ppm";
   #endif
+  #if s_KS0196
+    pinMode(ks0196_analog_pin, INPUT);
+    pinMode(ks0196_digital_pin, OUTPUT);
+    Data["KS0196"]["air_ppm"]["units"] = "ppm";
+  #endif
 }
 
 void loop(){
@@ -33,6 +48,16 @@ void loop(){
     int air_ppm = analogRead(A0);
   #endif
 
+  #if s_KS0196
+    for (int n = 0; n < N; n++){
+      digitalWrite(ks0196_digital_pin, LOW);
+      delayMicroseconds(ks0196_t1);
+      mean += analogRead(ks0196_analog_pin); 
+      digitalWrite(ks0196_digital_pin, HIGH); 
+      delayMicroseconds(ks0196_t2);
+    }
+  #endif
+
   long now = millis();
   if(now - last_now > dt){
     last_now = now;
@@ -42,6 +67,9 @@ void loop(){
     #endif
     #if s_MQ135
       Data["MQ135"]["air_ppm"]["value"] = air_ppm;
+    #endif
+    #if s_KS0196
+      Data["KS0196"]["air_ppm"]["value"] = mean / N;
     #endif
 
     Serial.println(JSON.stringify(Data));
